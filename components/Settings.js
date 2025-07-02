470da04e093b3ad9d46bc95b3e85fb445e84ca9d
sverghes <shiny.verghese@southwales.ac.uk>
2025-07-02 01:29:09 +0100

    Update Settings: Add detailed acknowledgements and update contact emails to USW investigators

--- components/Settings.js
+++ components/Settings.js
@@ -13,12 +13,12 @@ const Settings = (props) => {
 				<div className={styles.item} onClick={() => props.setOn(true)}><span>View onboarding</span> <img src="enter.svg"/></div>
 				<div className={styles.space}></div>
 				<a href="https://github.com/sverghes/WasteWarrior2" target="_blank" className={styles.item}><span>About this app</span> <img src="enter.svg"/></a>
-				<a href="https://github.com/sverghes/WasteWarrior2" target="_blank" className={styles.item}><span>Acknowledgements</span> <img src="enter.svg"/></a>
+				<a href="https://github.com/alyssaxuu/ecosnap?tab=readme-ov-file" target="_blank" className={styles.item}><span>Acknowledgements</span> <img src="enter.svg"/></a>
 				<div className={styles.space}></div>
 				<a href="https://ctmuhb.nhs.wales/hospitals/recycling/" target="_blank" className={styles.item}><span>CTMUHB Recycling</span> <img src="enter.svg"/></a>
 				<div className={styles.space}></div>
-				<a href="mailto:hi@alyssax.com" target="_blank" className={styles.item}><span>Contact us</span> <img src="enter.svg"/></a>
-				<div className={styles.credit}>Adapted from Eco-snap</div>
+				<a href="mailto:j.kulon@southwales.ac.uk;shiny.verghese@southwales.ac.uk" target="_blank" className={styles.item}><span>Contact us</span> <img src="enter.svg"/></a>
+				<div className={styles.credit}>This app is adapted from Eco-snap linked to https://github.com/alyssaxuu/ecosnap?tab=readme-ov-file. The app is specially used in the recycling project with CTMUHB. The principal investigators are Dr Shiny Verghese and Prof Janusz Kulon from the University of South Wales.</div>
 			</div>
 		</div>
 	)
